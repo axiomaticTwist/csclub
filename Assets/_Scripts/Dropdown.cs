f096985aece8d0cdf8ad00f38f9f8f692a5c9005
axiomaticTwist <shanekim28@gmail.com>
2019-09-14 22:54:35 -0700

    Lots of stuff
    Added many textures, added some scenes, added dropdown

---
+++ Assets/_Scripts/Dropdown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dropdown : MonoBehaviour {
+
+	public GameObject viewport;
+	public GameObject arrow;
+
+	public Text label;
+
+	private bool dropped = false;
+	private bool isAnimating = false;
+
+	public void PlayAnimations() {
+		if (!dropped) {
+			viewport.GetComponent<Animator>().SetBool("dropped", dropped);
+			arrow.GetComponent<Animator>().SetBool("dropped", dropped);
+		} else {
+			viewport.GetComponent<Animator>().SetBool("dropped", dropped);
+			arrow.GetComponent<Animator>().SetBool("dropped", dropped);
+		}
+
+		dropped = !dropped;
+	}
+
+
+}
