8e903b16bfb6a35e6b42bd9493d8cbb968eea98f
axiomaticTwist <shanekim28@gmail.com>
2020-01-18 20:28:17 -0800

    Integrated StudentVue scraping

---
+++ Assets/_Scripts/Gradebook.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+
+public static class Gradebook {
+	public static void Main(string[] args) {
+		string username = "";
+		string password = "";
+
+		string postString = String.Format("username={0}&amp;password={1}", username, password);
+
+		HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create("https://sis.powayusd.com/PXP2_Login_Student.aspx?regenerateSessionId=True");
+
+		httpRequest.Method = "POST";
+		httpRequest.ContentType = "application/x-www-form-urlencoded";
+
+		byte[] byteData = System.Text.Encoding.UTF8.GetBytes(postString);
+		httpRequest.ContentLength = byteData.Length;
+
+		Stream requestStream = httpRequest.GetRequestStream();
+		requestStream.Write(byteData, 0, byteData.Length);
+		requestStream.Close();
+
+		HttpWebResponse httpResponse = (HttpWebResponse) httpRequest.GetResponse();
+		Stream responseStream = httpResponse.GetResponseStream();
+
+		System.Text.StringBuilder sb = new System.Text.StringBuilder();
+		using (StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.UTF8)) {
+			string line;
+			while ((line = reader.ReadLine()) != null) {
+				sb.Append(line);
+			}
+		}
+
+		string serverResponse = sb.ToString();
+	}
+
+}
---
+++ Assets/test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StudentVueAPI;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		StudentVue studentVue = new StudentVue();
+		studentVue.Login("","","sis.powayusd.com");
+
+		foreach (var x in studentVue.GetGradeBook()) {
+			Debug.Log(x.CourseTitle);
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
