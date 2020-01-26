b4f39718b3b07dcc6356c92096ab0b541ee4c615
suhailsaqan <43693074+suhailsaqan@users.noreply.github.com>
2020-01-25 19:49:03 -0800

    Updated Home

---
+++ Assets/_Scripts/Date.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Date : MonoBehaviour
+{
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        string date = DateTime.Now.ToString("MMMM dd");
+        text.text = date;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
